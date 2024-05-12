d6862cc6581d17531da91a99aaed64c69a408932
Ismail Adeshola Ogundele <ismailadesholaharun@gmail.com>
2024-05-11 23:35:48 -0400

    React Pages created with Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 function App() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/forgotPassword" element={<ForgotPassword />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+      </Router>
     </>
   );
 }
