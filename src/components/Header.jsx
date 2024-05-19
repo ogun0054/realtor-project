2039da853ba6614e3cf7db0eacf565fb77af49db
Ismail Adeshola Ogundele <ismailadesholaharun@gmail.com>
2024-05-19 00:55:54 -0400

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,13 @@
+import { useLocation, useNavigate } from "react-router-dom";
 function Header() {
+  const location = useLocation();
+  // const navigate = useNavigate();
+  // console.log(location);
+  function pathMathRoute(route) {
+    if (route === location.pathname) {
+      return true;
+    }
+  }
   return (
     <div className="bg-white border-b-1 shadow-sm sticky top-0 z-50 h-10">
       <header className="flex justify-between items-center px-3 max-w-6xl mx-auto mt-5">
@@ -12,11 +21,26 @@ function Header() {
 
         <div>
           <ul className="flex space-x-10 ">
-            <li className="hover:bg-black-700 hover:border-b-2 hover:color-">
+            <li
+              className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent 
+              ${pathMathRoute("/") && "text-black border-b-red-500"}`}
+            >
               Home
             </li>
-            <li>Offers</li>
-            <li>SignIn</li>
+            <li
+              className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMathRoute("/offers") && "text-black border-b-red-500"
+              }`}
+            >
+              Offers
+            </li>
+            <li
+              className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMathRoute("/sign-in") && "text-black border-b-red-500"
+              }`}
+            >
+              Sign-in
+            </li>
           </ul>
         </div>
       </header>
